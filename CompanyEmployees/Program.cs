0025f0cab96d1a5dd78fb885c5f372d25f787ae7
Quang Duy <justinquangduy@gmail.com>
2024-04-06 15:46:40 +0700

    Working with the Use Method

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -31,8 +31,18 @@ app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("Hello from the middleware component.");
+    Console.WriteLine($"Logic before executing the next delegate in the Use method");
+    await next.Invoke();
+    Console.WriteLine($"Logic after executing the next delegate in the Use method");
+});
+
 app.Run(async context =>
 {
+    Console.WriteLine($"Writing the response to the client in the Run method");
+    context.Response.StatusCode = 200;
     await context.Response.WriteAsync("Hello from the middleware component");
 });
 
