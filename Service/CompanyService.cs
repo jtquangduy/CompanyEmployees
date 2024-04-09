9d2f4b3bb54320fd8662741036a586bd9ab7e265
Quang Duy <justinquangduy@gmail.com>
2024-04-09 17:43:50 +0700

    Getting a Single Resource From the Database

--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -26,5 +26,13 @@ namespace Service
 
             return companiesDto;
         }
+
+        public CompanyDto GetCompany(Guid id, bool trackChanges)
+        {
+            var company = _repository.Company.GetCompany(id, trackChanges);
+            //Check if the company is null
+            var companyDto = _mapper.Map<CompanyDto>(company);
+            return companyDto;
+        }
     }
 }
