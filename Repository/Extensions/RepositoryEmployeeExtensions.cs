501c2530c715d9d6235250ef21e24df70427b641
Quang Duy <justinquangduy@gmail.com>
2024-04-17 17:54:07 +0700

    Improving the Sorting Functionality

--- Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using Entities.Models;
+using Repository.Extensions.Utility;
 using System.Linq.Dynamic.Core;
-using System.Reflection;
-using System.Text;
 
 namespace Repository.Extensions
 {
@@ -25,29 +24,7 @@ namespace Repository.Extensions
             if (string.IsNullOrWhiteSpace(orderByQueryString))
                 return employees.OrderBy(e => e.Name);
 
-            var orderParams = orderByQueryString.Trim().Split(',');
-
-            var propertyInfos = typeof(Employee).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-
-            var orderQueryBuilder = new StringBuilder();
-            foreach (var param in orderParams)
-            {
-                if (string.IsNullOrWhiteSpace(param))
-                    continue;
-
-                var propertyFromQueryName = param.Split(" ")[0];
-
-                var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
-
-                if (objectProperty == null)
-                    continue;
-
-                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
-
-                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction},");
-            }
-
-            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+            var orderQuery = OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQueryString);
 
             if (string.IsNullOrWhiteSpace(orderQuery))
                 return employees.OrderBy(e => e.Name);
