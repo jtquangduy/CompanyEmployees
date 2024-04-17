998f20b9822cc20b2b149b5663b54fee36e7768c
Quang Duy <justinquangduy@gmail.com>
2024-04-17 15:03:01 +0700

    Filtering

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -13,8 +13,8 @@ namespace Repository
 
         public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
         {
-            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
-                .OrderBy(e => e.Name)
+            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId) && 
+                (e.Age >= employeeParameters.MinAge && e.Age <= employeeParameters.MaxAge), trackChanges)
                 .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize)
                 .Take(employeeParameters.PageSize)
                 .ToListAsync();
